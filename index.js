b2ff920e05457ebf7a1e61b81f78303ff7106122
Kim Kyungmin <qaws2051@gmail.com>
2020-11-24 02:29:56 +0900

    secret key protection

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://kmkim:dbrmdk56@boilerplate.0w9uq.mongodb.net/boilerplate?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/', (req, res) => res.send('Hhhello World!'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`Example app Hosted on port:${port}`));
